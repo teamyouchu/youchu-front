23b6ece491b3976ddea86b91d47650bd7262e5ed
jisu Seo <jseo9732@gmail.com>
2023-08-18 17:22:23 +0900

    Chore: api url 변경

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const apiUrl = 'https://api.youchu.io/v1';
+export const apiUrl = 'https://legacy-api.youchu.io/v1';
 // export const apiUrl = 'http://localhost:5000/v1';
 
 axios.defaults.baseURL = apiUrl;
